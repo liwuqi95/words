90c3af605bdcff8f316e1c203cfae66520a1345f
liwuqi95 <wuqi.li@mail.utoronto.ca>
2018-06-03 16:14:53 +0800

    change some alert translation

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -29,7 +29,7 @@ class WordsController < ApplicationController
 
     respond_to do |format|
       if @word.save
-        format.html { redirect_to words_path, notice: 'Word was successfully created.' }
+        format.html { redirect_to words_path, notice: '成功添加单词.' }
         format.json { render :show, status: :created, location: @word }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class WordsController < ApplicationController
   def update
     respond_to do |format|
       if @word.update(word_params)
-        format.html { redirect_to words_path, notice: 'Word was successfully updated.' }
+        format.html { redirect_to words_path, notice: '成功修改单词.' }
         format.json { render :show, status: :ok, location: @word }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class WordsController < ApplicationController
   def destroy
     @word.destroy
     respond_to do |format|
-      format.html { redirect_to words_url, notice: 'Word was successfully destroyed.' }
+      format.html { redirect_to words_url, notice: '成功删除单词.' }
       format.json { head :no_content }
     end
   end
