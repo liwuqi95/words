81e72a2f99f2865e2ee669833caad468d2cc7550
liwuqi95 <wuqi.li@mail.utoronto.ca>
2018-06-04 12:50:12 +0800

    translate word characters.

--- app/models/word_meaning.rb
+++ app/models/word_meaning.rb
@@ -1,6 +1,7 @@
 class WordMeaning < ApplicationRecord
   belongs_to :word
 
-  enum characters: [:non, :n, :vi, :vt, :pron, :adj, :adv, :num, :art, :prep, :conj, :int]
+  enum characters: ['无', '名词（n.）', '不及物动词（vi.)', '及物动词（vt.)', '代词(pron.)', '形容词（dj.）',
+                    '副词（adv.)', '数词（num.）', '冠词（art.）', '介词（prep.）', '连词（conj.）', '感叹词（int.)']
 
 end
