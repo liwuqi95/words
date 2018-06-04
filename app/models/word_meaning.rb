bb1081b375d736c9a11256ebf7c6bc9365efe82d
liwuqi95 <wuqi.li@mail.utoronto.ca>
2018-06-04 08:23:32 +0800

    minor bug fix.

--- app/models/word_meaning.rb
+++ app/models/word_meaning.rb
@@ -1,6 +1,6 @@
 class WordMeaning < ApplicationRecord
   belongs_to :word
 
-  enum characters: [:none, :n, :vi, :vt, :pron, :adj, :adv, :num, :art, :prep, :conj, :int]
+  enum characters: [:non, :n, :vi, :vt, :pron, :adj, :adv, :num, :art, :prep, :conj, :int]
 
 end
