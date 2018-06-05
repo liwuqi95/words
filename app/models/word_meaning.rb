faf0a49b9acfd599492b4607d4618d36c6902bf5
liwuqi95 <wuqi.li@mail.utoronto.ca>
2018-06-05 22:32:33 +0800

    disable csrf for now

--- app/models/word_meaning.rb
+++ app/models/word_meaning.rb
@@ -1,7 +1,7 @@
 class WordMeaning < ApplicationRecord
   belongs_to :word
 
-  enum character: ['无', '名词（n.）', '不及物动词（vi.)', '及物动词（vt.)', '代词(pron.)', '形容词（dj.）',
+  enum character: ['无', '名词（n.）', '动词（v.)', '代词(pron.)', '形容词（adj.）',
                     '副词（adv.)', '数词（num.）', '冠词（art.）', '介词（prep.）', '连词（conj.）', '感叹词（int.)']
 
 end
